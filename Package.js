6143bdad214385df234195fd0d2802d412322da0
ritesh07-create <riteshmhtre@gmail.com>
2026-04-13 14:45:22 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "esports-backend",
+  "version": "1.0.0",
+  "type": "commonjs",
+  "main": "api/index.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "firebase-admin": "^12.0.0",
+    "axios": "^1.6.7",
+    "node-cron": "^3.0.3"
+  }
+      }
+
